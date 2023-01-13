e84657d15a0ec6867e5d81e69ad3d0d297d66778
Ilya <iilavr@mail.ru>
2023-01-13 23:32:05 +0300

    add detail view and presenter

---
+++ HW22-CoreData/Sources/DetailScreen/DetailPresenter.swift
@@ -0,0 +1,43 @@
+//
+//  DetailPresenter.swift
+//  HW22-CoreData
+//
+//  Created by Илья on 13.01.2023.
+//
+
+import Foundation
+
+protocol DetailPresenterProtocol: AnyObject {
+    func setData()
+    func updateData(name: String?, dateOfBirth: String?, gender: String?, image: Data?)
+    
+    init(user: Person, view: DetailViewProtocol, router: RouterProtocol)
+}
+
+class DetailPresenter: DetailPresenterProtocol {
+    var person: Person
+    weak var view: DetailViewProtocol?
+    var router: RouterProtocol?
+    var model: ModelData?
+    
+    required init(user: Person, view: DetailViewProtocol, router: RouterProtocol) {
+        self.person = user
+        self.view = view
+        self.router = router
+    }
+    
+    func setData() {
+        view?.setupDetailedView(name: person.name, dateOfBirth: person.dateOfBirth, gender: person.gender, image: person.image)
+    }
+    
+    func updateData(name: String?, dateOfBirth: String?, gender: String?, image: Data?) {
+        person.name = name
+        person.dateOfBirth = dateOfBirth
+        person.gender = gender
+        person.image = image
+        person.addDate = Date()
+        model?.saveContext()
+    }
+}
+
+
