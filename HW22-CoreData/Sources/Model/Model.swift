03c51088f4acac46bce6888b1d18722cc64f6f12
Ilya <iilavr@mail.ru>
2023-01-13 23:27:07 +0300

    add main view and entity

---
+++ HW22-CoreData/Sources/Model/Model.swift
@@ -0,0 +1,53 @@
+//
+//  Model.swift
+//  HW22-CoreData
+//
+//  Created by Илья on 13.01.2023.
+//
+
+import UIKit
+
+class ModelData {
+    var persons: [Person] = []
+    
+    let context = (UIApplication.shared.delegate as!
+                   AppDelegate).persistentContainer.viewContext
+    
+    private var managedObject: Person {
+        Person(context: context)
+    }
+    
+    func saveContext () {
+        if context.hasChanges {
+            do {
+                print("Save \(context)")
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func getPersons() -> [Person] {
+        let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+        let sort = NSSortDescriptor(key: #keyPath(Person.addDate), ascending: true)
+        fetchRequest.sortDescriptors = [sort]
+        do {
+            persons = try context.fetch(fetchRequest)
+        } catch {
+            print("Cannot fetch Expenses")
+        }
+        return persons
+    }
+    
+    func addPerson(name: String) {
+        managedObject.name = name
+        saveContext()
+    }
+    
+    func delete(person: Person) {
+        context.delete(person)
+        saveContext()
+    }
+}
